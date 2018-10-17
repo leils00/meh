d60e013bcf071d73936e9650fa98f51a83991046
leils00 <43509443+leils00@users.noreply.github.com>
2018-10-16 22:10:02 -0400

    Update mehmeh.cpp

--- mehmeh.cpp
+++ mehmeh.cpp
@@ -6,14 +6,13 @@
 #include <string>
 #include <vector>
 using namespace std;
-
+//trying new things so there are a lot of things that probably don't work
 //declare functions
 int GetRandom(int min = 0, int max = 52);
-string DealCard(string cardPlayer);
-int ScoreHand(int totalscore);
+string DealCard();
+//int ScoreHand();
 
-int main()
-{
+int main(){
 	srand(time(0));
 	int playerNum;
 	cout << "How many players? ";
@@ -21,35 +20,21 @@ int main()
 
 	string PlayerCard;
 	int i;
-	for (i = 1; i <= playerNum; i++)
-	{
-		cout << "Player " << i << " has cards: " << DealCard(PlayerCard) << " " << DealCard(PlayerCard);
-		cout << setw(20) << "" << left << "(" << ScoreHand(0) << ")" << endl;
+	for (i = 1; i <= playerNum; i++){
+		cout << "Player " << i << " has cards: " << DealCard() << " " << DealCard();
+		cout << setw(20) << "" << left << "(" << /*ScoreHand() <<*/ ")" << endl;
 	}
 	
+	//visual studio only
 	int close;
 	cin >> close;
 
 	return 0;
 }
 
-//random number for what??? // please check the restrictions, if we use an array of 52 element to store those cards, then the random number will be index of array
-// The GetRandom funtion should return an integer
-void GetRandom(int min, int max)
 
-int GetRandom(int min, int max)
-{
-	for (int h = 0; h < 52; h++)
-	{
-		int hand = rand() % (max - h + 1);
-		return hand;
-	}
-}
-
-//deal cards to players // better to use an array of 52 elements to store cards
-string DealCard(string cardPlayer)
-{
-	//pairing values and suits
+string DeckOfCards() {
+	string cardDeck[52];
 	string cardFace;
 	string VALUES[13] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 	char SUITS[4] = { 'H', 'D', 'S', 'C' };
@@ -57,45 +42,80 @@ string DealCard(string cardPlayer)
 	int cardS = rand() % 4;
 	cardFace += cardV[VALUES];
 	cardFace += cardS[SUITS];
-	string cardDeck[52];
-	//how do I assign them to players and make sure there is not repetition???
 	return cardFace;
-	for (int c = 0; c < 52; c++)
-	{
-		if (GetRandom() <= 13)
-		{
-			return cardDeck[c];
-		}
+}
 
-		if (GetRandom() >= 14 && GetRandom() <= 26)
-		{
-			return cardDeck[c];
-		}
+int GetRandom(int min, int max){
+	
+	int cardDeck[52];
+	for (int d = 0; d < 52; d++) {
+		cardDeck[d] = max - rand() % (max - min + 1);
+		return cardDeck[d];
+	}	   
+}
 
-		if (GetRandom() >= 27 && GetRandom() <= 39)
-		{
-			return cardDeck[c];
-		}
-
-		if (GetRandom() >= 40 && GetRandom() <= 52)
-		{
-			return cardDeck[c];
-		}
+/*void shuffleDeck(int cardDeck[52], int n) {
+	for (int j = 0; j < n; j++) {
+		int shuffle = j + (rand() % (52 - j));
+		cardDeck[j] = cardDeck[n]; 
 	}
+}*/
+//deal cards to players
+string DealCard(){
+	//pairing values and suits
+	const int NumOfCards = 2;
+	string cardFace = DeckOfCards();
+	int cardDeck[52];
+	for (int j = 0; j < 52; cardDeck[j++]) {
+		cardDeck[j] = rand() % (52 - j) + j;
+		return cardDeck[j];
+	}
+	
+	
 } 
 
 //total scores
-int ScoreHand(int totalscore)
-{
+/*int ScoreHand() {
 	int val = 0;
-	switch (val)
-	{
-	case 'A':
-		totalscore += 1;
-		break;
-	case'2':
-		totalscore += 2;
-		break;
+	DealCard();
+	int totals = 0;
+	for (int v = 0; v < 52; v++) {
+		switch (cardDeck[v]) {
+		case 'A':
+			totals += 1;
+			break;
+		case '2':
+			totals += 2;
+			break;
+		case'3':
+			totals += 3;
+			break;
+		case '4':
+			totals += 4;
+			break;
+		case '5':
+			totals += 5;
+			break;
+		case '6':
+			totals += 6;
+			break;
+		case '7':
+			totals += 7;
+			break;
+		case '8':
+			totals += 8;
+			break;
+		case '9':
+			totals += 9;
+			break;
+		case '10':
+		case'J':
+		case 'Q':
+		case'K':
+			totals += 10;
+			break;
+		}
+		return totals;
+
 	}
-	return totalscore;
-}
+}*/
